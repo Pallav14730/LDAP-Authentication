0afb26b14c706a6b0d710d54c10d28bf43af66ef
Pallav Verma <81040573+Pallav14730@users.noreply.github.com>
2026-08-03 02:22:27 +0530

    Update endpoint.js

--- endpoint.js
+++ endpoint.js
@@ -1,1 +1,24 @@
 http://localhost:3000/api/report?fromDate=2026-07-01&toDate=2026-07-28
+<Grid size={{ xs: 12, md: 2 }}>
+  <TextField
+    fullWidth
+    select
+    size="small"
+    label="User Name"
+    value={username}
+    onChange={(e) => setUserName(e.target.value)}
+  >
+    <MenuItem value="">
+      Select UserName
+    </MenuItem>
+
+    {usernames.map((name: string) => (
+      <MenuItem
+        key={name}
+        value={name}
+      >
+        {name}
+      </MenuItem>
+    ))}
+  </TextField>
+</Grid>
