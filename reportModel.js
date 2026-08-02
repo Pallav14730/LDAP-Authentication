b7b3e7acbecbe8bacec0644c66b188c6c9771ec4
Pallav Verma <81040573+Pallav14730@users.noreply.github.com>
2026-08-02 23:35:47 +0530

    Update reportModel.js

--- reportModel.js
+++ reportModel.js
@@ -1,35 +1,21 @@
 const pool = require("../config/db");
 
-exports.getReport = async (fromDate, toDate, towerName, applicationName) => {
+exports.getReport = async (fromDate, toDate) => {
   const query = `
     SELECT
-      r.rl_user_id,
-      r.id,
-      r.location_type,
-      r.application_id,
-      r.application_name,
-      r.activity,
-      r.sub_activity,
-      r.description,
-      r.effort_hours,
-      r.tower_id,
-      r.tower_name,
-      r.group_id,
-      r.group_name,
-      r.created_date
-    FROM resource_loading_default r
-    WHERE r.created_date BETWEEN $1 AND $2
-      AND r.tower_name = $3
-      AND r.application_name = $4
-    ORDER BY r.created_date;
+      effort_date,
+      user_name,
+      application_name,
+      activity,
+      sub_activity,
+      description,
+      effort_hours
+    FROM resource_loading
+    WHERE effort_date BETWEEN $1 AND $2
+    ORDER BY effort_date ASC, user_name ASC;
   `;
 
-  const result = await pool.query(query, [
-    fromDate,
-    toDate,
-    towerName,
-    applicationName,
-  ]);
+  const result = await pool.query(query, [fromDate, toDate]);
 
   return result.rows;
 };
