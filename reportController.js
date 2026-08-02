bdb674c3010a41528149a249204af45967edd4e3
Pallav Verma <81040573+Pallav14730@users.noreply.github.com>
2026-08-02 23:36:17 +0530

    Simplify report fetching by removing unused parameters

--- reportController.js
+++ reportController.js
@@ -2,18 +2,15 @@ const reportModel = require("../models/reportModel");
 
 exports.downloadReport = async (req, res) => {
   try {
-    const { fromDate, toDate, tower, application } = req.query;
+    const { fromDate, toDate } = req.query;
 
-    const data = await reportModel.getReport(
-      fromDate,
-      toDate,
-      tower,
-      application
-    );
+    const data = await reportModel.getReport(fromDate, toDate);
 
     res.status(200).json(data);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: "Failed to fetch report" });
+    res.status(500).json({
+      message: "Failed to fetch report",
+    });
   }
 };
