88b80b489d87719e018de30e0ea704a0ba4de0ee
Pallav Verma <81040573+Pallav14730@users.noreply.github.com>
2026-08-02 23:36:48 +0530

    Update API endpoint date parameters

--- endpoint.js
+++ endpoint.js
@@ -1,1 +1,1 @@
-http://localhost:3000/api/report?fromDate=2026-08-01&toDate=2026-08-02&tower=Corp%20%26%20Commercial&application=Centricity
+http://localhost:3000/api/report?fromDate=2026-07-01&toDate=2026-07-28
